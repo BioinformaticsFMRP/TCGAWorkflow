de8dd85d3897e2802ffcf3322c868c1c2edc565f
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2017-01-13 15:06:55 -0800

    Adding utilies.R

---
+++ R/utilities.R
@@ -0,0 +1,161 @@
+#' Access biomart to get 
+#' @description getRNAseq is a function to download RNAseq data for all samples of a certain cancer type from TCGA
+#' @param disease A character specifies disease in TCGA such as BLCA
+#' @param basedir Download all RNA seq level 3 data for the specified disease.
+#' @param genome Data aligned against which genome of reference. Options: "hg19", "hg38" (default)
+#' @usage getRNAseq(disease, basedir = "./Data")
+#' @return Download all RNA seq level 3 data for the specified disease.
+#' @importFrom TCGAbiolinks GDCdownload GDCquery GDCprepare
+gaiaCNVplot <- function (calls, cancer=NULL, threshold=0.01) 
+{
+  Calls <- calls[order(calls[,"Region Start [bp]"]),]
+  Calls <- Calls[order(Calls[,"Chromosome"]),]
+  rownames(Calls) <- NULL
+  Chromo <- Calls[,"Chromosome"]
+  Gains <- apply(Calls,1,function(x) ifelse(x["Aberration Kind"]==1, x["score"], 0))
+  Losses <- apply(Calls,1,function(x) ifelse(x["Aberration Kind"]==0, x["score"], 0))
+  plot(Gains, ylim = c(-max(Calls[,"score"]+2), max(Calls[,"score"]+2)), type = "h", 
+       col = "red", xlab = "Chromosome", ylab = "Score", 
+       #main = paste("Recurrent Copy Number Variations",cancer, sep=" - "), 
+       xaxt = "n")
+  points(-(Losses), type = "h", col = "blue")
+  abline(h = 0, cex = 4)
+  abline(h = -log10(threshold), col = "orange", cex = 4, main="test")
+  abline(h = log10(threshold), col = "orange", cex = 4, main="test")
+  uni.chr <- unique(Chromo)
+  temp <- rep(0, length(uni.chr))
+  for (i in 1:length(uni.chr)) {
+    temp[i] <- max(which(uni.chr[i] == Chromo))
+  }
+  for (i in 1:length(temp)) {
+    abline(v = temp[i], col = "black", lty = "dashed", )
+  }
+  nChroms <- length(uni.chr)
+  begin <- c()
+  for (d in 1:nChroms) {
+    chrom <- sum(Chromo == uni.chr[d])
+    begin <- append(begin, chrom)
+  }
+  temp2 <- rep(0, nChroms)
+  for (i in 1:nChroms) {
+    if (i == 1) {
+      temp2[1] <- (begin[1] * 0.5)
+    }
+    else if (i > 1) {
+      temp2[i] <- temp[i - 1] + (begin[i] * 0.5)
+    }
+  }
+  uni.chr[uni.chr==23] <- "X"
+  uni.chr[uni.chr==24] <- "Y"
+  for (i in 1:length(temp)) {
+    axis(1, at = temp2[i], labels = uni.chr[i], cex.axis = 1)
+  }
+  legend(x=1,y=max(Calls[,"score"]+2), y.intersp=0.8, c("Amp"), pch=15, col=c("red"), text.font=3)
+  legend(x=1,y=-max(Calls[,"score"]+0.5), y.intersp=0.8, c("Del"), pch=15, col=c("blue"), text.font=3)
+}
+
+#' Get a matrix of interactions of genes from biogrid
+#' @description 
+#' Using biogrid database, it will create a matrix of gene interations.
+#' If columns A and row B has value 1, it means the gene A and gene B interatcs.
+#' @param tmp.biogrid Biogrid table
+#' @param names.genes List of genes to filter from output. Default: consider all genes
+#' @return A matrix with 1 for genes that interacts, 0 for no interaction.
+get.adjacency.biogrid <- function(tmp.biogrid, names.genes = NULL){
+  
+  if(is.null(names.genes)){
+    names.genes <- sort(union(unique(tmp.biogrid[,"Official.Symbol.Interactor.A"]), 
+                              unique(tmp.biogrid[,"Official.Symbol.Interactor.B"])))
+    ind <- seq(1,nrow(tmp.biogrid))
+  }else{
+    ind.A <- which(tmp.biogrid[,"Official.Symbol.Interactor.A"] %in% names.genes)
+    ind.B <- which(tmp.biogrid[,"Official.Symbol.Interactor.B"] %in% names.genes)
+    ind <- intersect(ind.A, ind.B)
+  }
+  
+  mat.biogrid <- matrix(0, nrow=length(names.genes), ncol=length(names.genes), dimnames=list(names.genes, names.genes))
+  
+  for(i in ind){
+    mat.biogrid[tmp.biogrid[i,"Official.Symbol.Interactor.A"], tmp.biogrid[i,"Official.Symbol.Interactor.B"]] <- mat.biogrid[tmp.biogrid[i,"Official.Symbol.Interactor.B"], tmp.biogrid[i,"Official.Symbol.Interactor.A"]] <- 1
+  }
+  diag(mat.biogrid) <- 0
+  
+  return(mat.biogrid)
+}
+
+#' Get GDC samples with both DNA methylation and Gene expression data
+#' @description 
+#' For a given TCGA project it gets the  samples (barcode) with both DNA methylation and Gene expression data
+#' @param project A GDC project
+#' @param n Number of samples to return. If NULL return all (default)
+#' @usage matched_met_exp("TCGA-ACC)
+#' @return A vector of barcodes
+#' @importFrom TCGAbiolinks  GDCquery
+matched_met_exp <- function(project, n = NULL){
+  # get primary solid tumor samples: DNA methylation
+  message("Download DNA methylation information")
+  met450k <- GDCquery(project = project,
+                      data.category = "DNA methylation",
+                      platform = "Illumina Human Methylation 450",
+                      legacy = TRUE, 
+                      sample.type = c("Primary solid Tumor"))
+  met450k.tp <-  met450k$results[[1]]$cases
+  
+  # get primary solid tumor samples: RNAseq
+  message("Download gene expression information")
+  exp <- GDCquery(project = project,
+                  data.category = "Gene expression",
+                  data.type = "Gene expression quantification",
+                  platform = "Illumina HiSeq", 
+                  file.type  = "results", 
+                  sample.type = c("Primary solid Tumor"),
+                  legacy = TRUE)
+  exp.tp <-  exp$results[[1]]$cases
+  print(exp.tp[1:10])
+  # Get patients with samples in both platforms
+  patients <- unique(substr(exp.tp,1,15)[substr(exp.tp,1,12) %in% substr(met450k.tp,1,12)] )
+  if(!is.null(n)) patients <- patients[1:n] # get only n samples
+  return(patients)
+}
+
+#' Evaluate survival based on the levels of TF expression
+#' @description 
+#' This function is an auxiliar function used to integrate ELMER and TCGAbiolinks pacakges.
+#' For a given regulatory TF found by ELMER, we will use TCGAbiolinks to get the clinical data of the
+#' samples and evaluate the survival for two groups, ones with higher expression of that TF and ones
+#' with lower expression. 
+#' @param TF A transcription factor
+#' @param mee A mee object from ELMER
+#' @param clinical Clinical data from GDC
+#' @param percentage Percentage of samples of each group. Default 30% in the higher expression 
+#' group and  30% in the lower expression group
+#' @return A pdf wuth the survival plot
+#' @importFrom TCGAbiolinks  TCGAanalyze_survival
+#' @importFrom ELMER  getGeneID getExp
+TCGAsurvival_TFplot <- function(TF,mee, clinical, percentage = 0.3){
+  
+  # For the transcription factor, gets it getGeneID
+  gene <- getGeneID(mee,symbol=TF)
+  # Get the expression values for the genes.
+  # (getExp is a ELMER function)
+  exp <- getExp(mee,geneID=gene)
+  
+  # Get the names of the 30% patients with lower expression
+  g1 <- names(sort(exp)[1:ceiling(length(exp) * percentage)])
+  
+  # Get the names of the 30% patients with higher expression
+  g2 <- names(sort(exp,decreasing = T)[1:ceiling(length(exp) * percentage)])
+  
+  # get the data of only these patients
+  idx <- clinical$bcr_patient_barcode %in% substr(c(g1, g2),1,12)
+  clinical <- clinical[idx,]
+  # Create the labels for each sample
+  clinical$tf_groups <- "high"
+  low.idx <- clinical$bcr_patient_barcode %in% substr(c(g1),1,12) 
+  clinical[low.idx,]$tf_groups <- "low"
+  
+  # Use TCGAbiolinks to create the survival curve
+  TCGAanalyze_survival(clinical,"tf_groups",
+                       legend=paste0(TF," Exp level"),
+                       filename = paste0(TF,".pdf"))
+}
