150d09bde0828def3e8a56a1e8f9eb2760c1c530
tiagochst <tiagochst@gmail.com>
2017-02-26 17:15:26 -0300

    Fixing DLL errors

--- R/TCGAWorkflow.R
+++ R/TCGAWorkflow.R
@@ -21,15 +21,5 @@
 #'
 #' @docType package
 #' @name TCGAbiolinks
-#' @import ELMER  downloader SummarizedExperiment  TCGAWorkflowData
-#' @import gaia ChIPseeker ComplexHeatmap
-#' @import minet c3net biomaRt pathview
-#' @import BSgenome.Hsapiens.UCSC.hg19 motifStack
-#' @import circlize pbapply GenomeInfoDb ggplot2 ggthemes parallel
-#' @importFrom rGADEM GADEM nOccurrences getPWM consensus nMotifs
-#' @importFrom MotIV viewAlignments motifMatch 
-#' @importFrom AnnotationHub query AnnotationHub
-#' @importFrom MotifDb MotifDb
-#' @importFrom clusterProfiler bitr
-#' @import DT RTCGAToolbox
+#' @import TCGAWorkflowData
 NULL
