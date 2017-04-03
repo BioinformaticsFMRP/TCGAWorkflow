0481920d2f5c5c446507056efed9334f050af8b0
tiagochst <tiagochst@gmail.com>
2017-04-03 00:45:06 -0300

    Fixing namespace

--- R/TCGAWorkflow.R
+++ R/TCGAWorkflow.R
@@ -26,11 +26,11 @@
 #' @import ELMER  downloader SummarizedExperiment  TCGAWorkflowData knitr
 #' @import gaia ChIPseeker ComplexHeatmap
 #' @import minet c3net biomaRt pathview
-#' @import  motifStack
+#' @import  motifStack TCGAbiolinks graphics
 #' @import circlize pbapply GenomeInfoDb ggplot2 ggthemes parallel
 #' @importFrom rGADEM GADEM nOccurrences getPWM consensus nMotifs
 #' @importFrom MotIV viewAlignments motifMatch 
 #' @importFrom AnnotationHub query AnnotationHub
 #' @importFrom clusterProfiler bitr
-#' @import DT 
+#' @import DT maftools
 NULL
