e5fe017a8b4a1f6d93c9a11dca54db639b13ecfe
Tiago Chedraoui Silva <tiagochst@usp.br>
2017-12-07 19:51:57 +0000

    Adding maftools
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/workflows/TCGAWorkflow@132234 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/TCGAWorkflow.R
+++ R/TCGAWorkflow.R
@@ -24,7 +24,7 @@
 #' @docType package
 #' @name TCGAWorkflow
 #' @import ELMER  downloader SummarizedExperiment  TCGAWorkflowData knitr
-#' @import gaia ChIPseeker ComplexHeatmap
+#' @import gaia ChIPseeker ComplexHeatmap maftools
 #' @import minet c3net biomaRt pathview
 #' @import  motifStack TCGAbiolinks graphics pander
 #' @import circlize pbapply GenomeInfoDb ggplot2 ggthemes parallel
